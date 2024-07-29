b0bb7d25bbde4e09456625c2f7b74bcad43843fe
Oluwaseyi <kdom22033@gmail.com>
2024-07-29 12:25:12 +0100

    Add files via upload

---
+++ BankApp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BankApp {
+    public static void main(String[] args) {
+
+//        Steps
+//        1. Have an array of user with their name, account ID, Pin, Account balance
+//        2. The users will be able to create account, check balance, deposit, withdraw and exit the program.
+//        3. The user will have to use their pin to login. If the pin is correct, their Acc no, name and balance will be shown.
+//        4. The user will be asked for the operation to be performed and then proceed.
+//        5. When the operation has been executed, the user will have an option to exit or continue.
+
+        System.out.println("WELCOME TO THRIVELINK BANK!");
+        System.out.println("We are glad that you chose to bank with us.");
+        System.out.println("You can perform the following operations on this app: Create an account, Check Account balance, Deposit and Withdraw.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter account number: ");
+    }
+}
